c233c5a2565309863f86f39ecc1842196e447957
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-11 14:15:11 +0530

    Prepare app for Render deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
-};
+  await mongoose.connect(process.env.MONGO_URI).then(()=>
+    console.log("MongoDB Connected")).catch(err=>
+      console.log(err));
+    
+  };
+  // console.log("MongoDB Connected");
